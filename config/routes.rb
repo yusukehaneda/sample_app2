da9772368f7821f1347449767722c7279e7b53fc
yusuke haneda <yusukehaneda0824@gmail.com>
2020-05-12 09:43:57 +0900

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  # => StaticPages#home
-  get 'static_pages/help'
+  root 'static_pages#home'
+  get  'static_pages/home'
+  # =>  StaticPages#home
+  get  'static_pages/help'
   # => StaticPages#help
-  root 'application#hello'
+
+  get  'static_pages/about'
+  # => StaticPages#about
+  #root 'application#hello'
 end
