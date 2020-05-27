de50b9d4bf66980267d34470c4e9bff322d96f38
yusuke haneda <yusukehaneda0824@gmail.com>
2020-05-27 21:51:06 +0900

    finish ch06

---
+++ test/models/user_test.rb
@@ -0,0 +1,78 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+
+ #テストの最初に必ず実行するのがsetup
+  def setup
+    @user = User.new(name: "Example User", 
+                    email: "user@example.com",
+                 password: "foobar", 
+                 password_confirmation: "foobar")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  #空文字だったら失敗してほしい @user.validがfalseになってほしい
+  test "name should be present" do
+    @user.name = "     "
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "     "
+    assert_not @user.valid?
+  end
+
+  #文字数の判定　長すぎたらfalseがになってほしい
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  #適切なアドレス
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  # 不適切なアドレス => 弾くべき=>falseになってほしい
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+  #ユーザーを重複させてみる
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  #空白6文字ではどうか
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  #5文字ならどうか
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+
+end
