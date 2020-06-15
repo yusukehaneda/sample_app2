1a19d8843ce846c3a194f5467530b2667b510aad
yusuke haneda <yusukehaneda0824@gmail.com>
2020-06-15 09:07:27 +0900

    finish ch13.4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,6 +104,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.active_storage.service = :amazon
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
@@ -125,6 +126,4 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  # アップロードされたファイルをAWSに保存する
-  config.active_storage.service = :amazon
 end
