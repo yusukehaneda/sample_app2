bcdf5bf6ce331c33db5dee0018ccc1b394385647
yusuke haneda <yusukehaneda0824@gmail.com>
2020-06-17 22:18:41 +0900

    finish ch14

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,23 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
+  #Default: foreign_key: "user_id"
+  # => '#{Model Name}s"となるとデフォルトでActiveRelationshipクラスを見に行ってしまうので、明記
+  has_many :active_relationships,  class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                    dependent: :destroy
+
+  has_many :passive_relationships, class_name: "Relationship",
+                                  foreign_key: "followed_id",
+                                    dependent: :destroy
+
+  # @user.active_relationships.map(&:followed)をfollowingだけで呼び出したい
+  has_many :following,                through: :active_relationships,
+                                       source: :followed
+
+  has_many :followers,                through: :passive_relationships, 
+                                       source: :follower
+
+
   attr_accessor :remember_token,
                 :activation_token, 
                 :reset_token
@@ -84,7 +102,29 @@ class User < ApplicationRecord
   # 試作feedの定義
   # 完全な実装は次章の「ユーザーをフォローする」を参照
   def feed
-    Micropost.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: self.id)
+    #Micropost.where("user_id IN (:following_ids) OR user_id = :user_id",
+    # following_ids: following_ids, user_id: self.id)
+    #Micropost.where("user_id IN (?) OR user_id = ?", following_ids, self.id)
+    #Micropost.where("user_id = ?", id)
+  end
+
+  # ユーザーをフォローする
+  def follow(other_user)
+    following << other_user
+  end
+
+  # ユーザーをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -25,10 +25,19 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
 
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+     # GET /users/1/following
+     # GET /users/1/followers
+    end
+  end
+
+
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 
 end
 
